77903ffb2ed8d4d86a47381e65d9a5fb93c32452
KAVITHA <kavithakumaris@gmail.com>
2022-08-28 15:28:14 +0000

    i am changing feature level changes and only show up in feature branch now

--- file.java
+++ file.java
@@ -1,4 +1,7 @@
 change
 <<<<<<< HEAD
  am changing my filesthis is a change from local
-my feature branch chnges are these - removed few lines from main file and added these lines
+my feature branch chnges are these - removed few lines from main file and added these lins 
+my new changes are done at feature 
+my updated changes
+vim lerned short cuts again
